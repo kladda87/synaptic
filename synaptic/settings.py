97c5efdec4d35b57688f463ba67c045ed09df767
kladda <gleiflandberntsson@gmail.com>
2023-10-04 11:57:51 +0200

    Updated secret key

--- synaptic/settings.py
+++ synaptic/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4ed+5_0m_$2)t=lj7--la^-@^kxoo0tut20mo-#l^oyx&=t=(g'
+
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
