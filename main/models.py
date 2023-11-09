96090e3cd81f79bbc094613409120003e00ab6f4
kladda <gleiflandberntsson@gmail.com>
2023-11-09 16:41:02 +0100

    Increased allowed # of characters in several model fields

--- main/models.py
+++ main/models.py
@@ -13,12 +13,12 @@ class FAQ(models.Model):
 class Staff(models.Model):
     name = models.CharField(max_length=100, default='Name')
     title = models.CharField(max_length=100, default='Title')
-    description = models.TextField(max_length=500, default='Description')
+    description = models.TextField(max_length=2000, default='Description')
     staff_image = models.ImageField(upload_to='staff_images/', null=True, blank=True)
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
     email_address = models.EmailField()
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     message = models.TextField()
